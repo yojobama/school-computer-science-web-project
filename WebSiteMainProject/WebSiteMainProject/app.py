f4329aeb7c3e0f7d24ba5f5faefeb896127bef5e
John <yojobama@gmail.com>
2025-01-27 10:49:28 +0200

    adding stuff :)

--- WebSiteMainProject/WebSiteMainProject/app.py
+++ WebSiteMainProject/WebSiteMainProject/app.py
@@ -3,7 +3,7 @@ This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
 
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
@@ -12,14 +12,26 @@ wsgi_app = app.wsgi_app
 
 @app.route('/')
 def hello():
-    """Renders a sample page."""
-    return "Hello World!"
+    return render_template('index.html')
+
+@app.route('/home')
+def home():
+    return render_template('index.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 
 if __name__ == '__main__':
     import os
     HOST = os.environ.get('SERVER_HOST', 'localhost')
     try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+        PORT = int(os.environ.get('SERVER_PORT', '8175'))
     except ValueError:
-        PORT = 5555
+        PORT = 8175
     app.run(HOST, PORT)
---
+++ WebSiteMainProject/WebSiteMainProject/static/main.js
@@ -0,0 +1,9 @@
+function openForm(form) {
+    document.getElementById(form).style.display = "block";
+}
+function closeForm(form) {
+    document.getElementById(form).style.display = "none";
+}
+function openPage(pageName) {
+    window.location.href = pageName;
+}
