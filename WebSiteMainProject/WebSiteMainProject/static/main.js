60eff3bcd71b0ed0a9e58895e49a57f30e35f937
John <yojobama@gmail.com>
2025-01-29 13:43:59 +0200

    stuff

--- WebSiteMainProject/WebSiteMainProject/static/main.js
+++
@@ -1,9 +0,0 @@
-function openForm(form) {
-    document.getElementById(form).style.display = "block";
-}
-function closeForm(form) {
-    document.getElementById(form).style.display = "none";
-}
-function openPage(pageName) {
-    window.location.href = pageName;
-}
